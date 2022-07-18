3c15779a3e7f5a5fce2a0fed1d74fad903205f02
Alexandr0 Jose dos Santos (P) <X292520@prservicos.com.br>
2022-07-18 11:13:20 -0300

    versao inicial projeto quarkus

---
+++ src/test/java/br/com/ajs/demo/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.ajs.demo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
